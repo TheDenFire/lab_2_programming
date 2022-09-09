7bd6314d3230bbfec4599c5c4f97822619b3a3ba
Bogdan <bog040040@yandex.ru>
2022-09-09 17:21:46 +0300

    1

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    double a;
+    float b,c1,c2, pi;
+    scanf("%lf",&a);
+    pi = 3.14;
+    c1 = (sin(2*a) + sin(5 * a) - sin(3*a)) /cos(a) + 1 - 2 * pow(sin(2*a),2);
+    c2 = 2 * sin(a);
+    printf("%lf %lf",c1,c2);
+    return 0;
+}
+
